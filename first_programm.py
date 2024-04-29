eba0577bb1b25bbcbe3d2032d7ccc89d2b74e943
AlexKhovatov <sanyhov2015@gmail.com>
2024-04-30 00:45:58 +0300

    Зодиак(1-е решение)

--- first_programm.py
+++ first_programm.py
@@ -1,5 +1,27 @@
+# Зодиак
+year = int(input())
 
-
-
-
-
+if (year - 2000) % 12 == 0:
+    print('Дракон')
+if (year - 2001) % 12 == 0:
+    print('Змея')
+if (year - 2002) % 12 == 0:
+    print('Лошадь')
+if (year - 2003) % 12 == 0:
+    print('Овца')
+if (year - 2004) % 12 == 0:
+    print('Обезьяна')
+if (year - 2005) % 12 == 0:
+    print('Петух')
+if (year - 2006) % 12 == 0:
+    print('Собака')
+if (year - 2007) % 12 == 0:
+    print('Свинья')
+if (year - 2008) % 12 == 0:
+    print('Крыса')
+if (year - 2009) % 12 == 0:
+    print('Бык')
+if (year - 2010) % 12 == 0:
+    print('Тигр')
+if (year - 2011) % 12 == 0:
+    print('Заяц')
